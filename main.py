e1ece3b40d9d96035ba459d85f7f6b540cffcbd4
JT <183244240+rtajt@users.noreply.github.com>
2025-08-11 03:49:10 -0400

    Convert everything to match statements in advance

--- main.py
+++ main.py
@@ -19,15 +19,17 @@ def convert_seconds_menu():
 
 def time_conversion_menu():
     choice=int(input("What type of time unit are you trying to convert?\n0:Seconds\n"))
-    if choice == 0:
-        convert_seconds_menu()
-    else:
-        print("Invalid time conversion")
+    match choice:
+        case 0:
+            convert_seconds_menu()
+        case _:
+            print("Invalid time conversion")
 
 def main():
     choice = int(input("What type of conversions would you like to do?\n0: Time\n"))
-    if choice == 0:
-        time_conversion_menu()
-    else:
-        print("Invalid conversion type")
+    match choice:
+        case 0:
+            time_conversion_menu()
+        case _:
+            print("Invalid conversion type")
 main()
