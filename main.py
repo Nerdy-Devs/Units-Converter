84218720c7e68e3ba92504c85559c455e36aad44
JT <183244240+rtajt@users.noreply.github.com>
2025-08-11 02:41:06 -0400

    Rewrite if statements into a match statement

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from timefunctions import *
 def convert_seconds_menu():
     choice = int(input("What do you want to convert seconds to?\n0:Minutes\n1:Hours\n2:Days\n3:Weeks\n"))
     secs = int(input("How many seconds would you like to convert?: "))
-    if choice == 0:
-        seconds_to_minutes(secs)
-    elif choice == 1:
-        seconds_to_hours(secs)
-    elif choice == 2:
-        seconds_to_days(secs)
-    elif choice == 3:
-        seconds_to_weeks(secs)
-    else:
-        print("Invalid seconds conversion")
+    match choice:
+        case 0:
+            seconds_to_minutes(secs)
+        case 1:
+            seconds_to_hours(secs)
+        case 2:
+            seconds_to_days(secs)
+        case 3:
+            seconds_to_weeks(secs)
+        case _:
+            print("Invalid seconds conversion")
+            
 def time_conversion_menu():
     choice=int(input("What type of time unit are you trying to convert?\n0:Seconds\n"))
     if choice == 0:
