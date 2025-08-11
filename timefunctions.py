ec0643107942237654efedab5fe3b885f2aa2eba
JT <183244240+rtajt@users.noreply.github.com>
2025-08-11 03:46:52 -0400

    Make tests a match statement as well

--- timefunctions.py
+++ timefunctions.py
@@ -28,18 +28,24 @@ def seconds_to_months(secs):
 # Love, James <3
 
 if __name__ == "__main__":
-    # Seconds tests
-    print(seconds_to_minutes(60))
-    print(seconds_to_minutes(81)) # Should return 1m21s
-    print(seconds_to_minutes(96)) # Should return 1m36s
-    print(seconds_to_hours(3600))
-    print(seconds_to_hours(953)) # Should return 0h15m53s
-    print(seconds_to_hours(7232)) # Should return 2h0m32s
-    print(seconds_to_days(86400))
-    print(seconds_to_days(9073)) # Should return 0d2h31m13s
-    print(seconds_to_days(57993)) # Should return 0d16h6m33s
-    print(seconds_to_weeks(604800))
-    print(seconds_to_weeks(970893)) # Should return 1w4d5h41m33s
-    print(seconds_to_weeks(95498)) # Should return 0w1d2h31m38s
-    # TODO: Add test cases for seconds -> months
-    # print(seconds_to_months(2592000))
+    test = int(input("Which test do you have the displeasure of running?\n0:Seconds\n"))
+    match test:
+        case 0:
+            print(seconds_to_minutes(60))
+            print(seconds_to_minutes(81)) # Should return 1m21s
+            print(seconds_to_minutes(96)) # Should return 1m36s
+            print(seconds_to_hours(3600))
+            print(seconds_to_hours(953)) # Should return 0h15m53s
+            print(seconds_to_hours(7232)) # Should return 2h0m32s
+            print(seconds_to_days(86400))
+            print(seconds_to_days(9073)) # Should return 0d2h31m13s
+            print(seconds_to_days(57993)) # Should return 0d16h6m33s
+            print(seconds_to_weeks(604800))
+            print(seconds_to_weeks(970893)) # Should return 1w4d5h41m33s
+            print(seconds_to_weeks(95498)) # Should return 0w1d2h31m38s
+            # TODO: Add test cases for seconds -> months
+            # print(seconds_to_months(2592000))
+            # print(seconds_to_months(2592000))
+            # print(seconds_to_months(2592000))
+        case _:
+            print("Wrong test dumbass")
