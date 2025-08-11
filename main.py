9bd08997dd922df54d56d9e4df3a37fcda67c124
JT <183244240+rtajt@users.noreply.github.com>
2025-08-11 01:17:28 -0400

    Refactoring code for better readability (I barely had to use AI for this step!)

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from timefunctions import *
 
-converter = int(input("What type of conversions would you like to do?\n0: Time\n"))
-
-if converter == 0:
-    timeconverter=int(input("What type of time conversion would you like to do?\n0:Seconds to Minutes"))
-    if timeconverter == 0:
+def convert_seconds_menu():
+    choice = int(input("What do you want to convert seconds to?\n0:Minutes"))
+    if choice == 0:
         secs = int(input("How many seconds would you like to convert? "))
         seconds, minutes = seconds_to_minutes(secs)
         print(str(secs) + " seconds is " + str(minutes) + " minutes and " + str(seconds) + " seconds.")
+def time_conversion_menu():
+    choice=int(input("What type of time unit are you trying to convert?\n0:Seconds"))
+    if choice == 0:
+        convert_seconds_menu()
     else:
         print("Invalid time conversion")
-else:
-    print("Invalid type of conversion")
+
+def main():
+    choice = int(input("What type of conversions would you like to do?\n0: Time\n"))
+    if choice == 0:
+        time_conversion_menu()
+    else:
+        print("Invalid conversion type")
+main()
