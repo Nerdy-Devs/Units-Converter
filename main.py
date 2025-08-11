cd1c462ac9f7dc6ceb047753cb6dffecb68ac6d8
JT <183244240+rtajt@users.noreply.github.com>
2025-08-11 01:33:57 -0400

    Add seconds to hours conversion

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from timefunctions import *
 
 def convert_seconds_menu():
-    choice = int(input("What do you want to convert seconds to?\n0:Minutes"))
+    choice = int(input("What do you want to convert seconds to?\n0:Minutes\n1:Hours\n"))
+    secs = int(input("How many seconds would you like to convert? "))
     if choice == 0:
-        secs = int(input("How many seconds would you like to convert? "))
         seconds, minutes = seconds_to_minutes(secs)
         print(str(secs) + " seconds is " + str(minutes) + " minutes and " + str(seconds) + " seconds.")
+    elif choice == 1:
+        seconds, minutes, hours = seconds_to_hours(secs)
+        print(str(secs) + " seconds is " + str(hours) + " hours, " + str(minutes) + " minutes and " + str(seconds) + " seconds.")
+    else:
+        print("Invalid seconds conversion")
 def time_conversion_menu():
-    choice=int(input("What type of time unit are you trying to convert?\n0:Seconds"))
+    choice=int(input("What type of time unit are you trying to convert?\n0:Seconds\n"))
     if choice == 0:
         convert_seconds_menu()
     else:
--- timefunctions.py
+++ timefunctions.py
@@ -4,3 +4,7 @@ def seconds_to_minutes(secs):
     minutes = math.floor(secs / 60)
     seconds = secs % 60
     return minutes, seconds
+def seconds_to_hours(secs):
+    hours = math.floor(secs / 3600)
+    minutes, seconds = seconds_to_minutes(secs % 3600)
+    return hours, minutes, seconds
